240a7ca1530f3e012c15a5bcf0d1cb0953a8ab9a
Shruti26122003 <shrutim8141@gmail.com>
2025-01-29 00:03:52 +0530

    login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { routes } from '../../app.routes';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  loginObj:any = {
+      "userName": "",
+      "password": ""
+  };
+  http = inject(HttpClient);
+  router = inject(Router);
+  onLogin(){
+    debugger;
+    this.http.post("https://projectapi.gerasim.in/api/EmployeeManagement/login",this.loginObj).subscribe((res:any)=>{
+      if(res.result){
+        localStorage.setItem('employeeApp',JSON.stringify(res.data));
+        this.router.navigateByUrl('dashboard');
+      } else{
+        alert(res.message)
+      }
+    })
+  }
 }
+
